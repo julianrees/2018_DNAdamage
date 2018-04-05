745c8a070983a88b86f17ddd6536235a36bf6be8
Julian Rees <julianrees@users.noreply.github.com>
2018-04-04 19:02:36 -0700

    re-wrote script for indexed lists for data sets

--- analyzeFACS.R
+++ analyzeFACS.R
@@ -5,104 +5,55 @@ library(ggcyto)
 library(flowCore)
 library(outliers)
 
-source("https://bioconductor.org/biocLite.R")
-biocLite()
+# source("https://bioconductor.org/biocLite.R")
+# biocLite()
 
 setwd('./')
 #---- Data import ----
-data_4h_BT549_gH2aX_S1_CTRL <- read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S1 CTRL.csv")[,3]
-data_4h_BT549_gH2aX_S1_HD <- read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S1 HD.csv")[,3]
-# data_4h_24h_BT549_gH2aX_S1_LD <- read.csv("Data/4h_24h BT549  gH2aX/4h_24h BT549  gH2aX_S1 LD.csv")[,3]
-data_4h_BT549_gH2aX_S2_CTRL <- read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S2 CTRL.csv")[,3]
-data_4h_BT549_gH2aX_S2_HD <- read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S2 HD.csv")[,3]
-# data_4h_24h_BT549_gH2aX_S2_LD <- read.csv("Data/4h_24h BT549  gH2aX/4h_24h BT549  gH2aX_S2 LD.csv")[,3]
-#data_4h_BT549_gH2aX_S3_CTRL <- read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S3 CTRL.csv")[,3]
-# data_4h_24h_BT549_gH2aX_S3_HD <- read.csv("Data/4h_24h BT549  gH2aX/4h_24h BT549  gH2aX_S3 HD.csv")[,3]
-# data_4h_24h_BT549_gH2aX_S3_LD <- read.csv("Data/4h_24h BT549  gH2aX/4h_24h BT549  gH2aX_S3 LD.csv")[,3]
+# Set 1 data, Control, High, Low, Set 2 data, same order
+rdata <- list()
+rdata[[1]] <- c(read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S1 CTRL.csv")[,3])
+rdata[[2]] <- c(read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S1 HD.csv")[,3])
+rdata[[3]] <- c(read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S1 LD.csv")[,3])
+rdata[[4]] <- c(read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S2 CTRL.csv")[,3])
+rdata[[5]] <- c(read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S2 HD.csv")[,3])
+rdata[[6]] <- c(read.csv("Data/4h_gH2aX_BT549/4h_gH2aX_BT549_S2 LD.csv")[,3])
 
-S1_CTRL <- data.frame(data_4h_BT549_gH2aX_S1_CTRL)
-colnames(S1_CTRL) <- "Alexa488"
+#---- Data processing ----
+logdata <- list()
+logmedians <- list()
+for (i in seq(length(rdata))){
+  logdata[[i]] <- signif(log(rdata[[i]]), digits = 4)
+  logmedians[[i]] <- median(logdata[[i]])
+}
 
-S1_CTRL$Alexa488 <- signif(log(S1_CTRL$Alexa488), digits = 4)
-
-ggplot(data = S1_CTRL, aes(x =Alexa488)) + 
-  geom_bar()
-
-median(S1_CTRL$Alexa488)
-
-
-#---- S2 Control ----
-S2_CTRL <- data.frame(data_4h_BT549_gH2aX_S2_CTRL)
-colnames(S2_CTRL) <- "Alexa488"
-
-S2_CTRL$Alexa488 <- signif(log(S2_CTRL$Alexa488), digits = 4)
-
-ggplot(data = S2_CTRL, aes(x =Alexa488)) + 
-  geom_bar()
-
-median(S2_CTRL$Alexa488)
-
-#---- Master control ----
 # make the average of the medians of the control data sets
-ctrl_abs_mean <- mean(c(median(S1_CTRL$Alexa488), median(S2_CTRL$Alexa488)))
+ctrl_abs_mean <- mean(c(logmedians[[1]], logmedians[[4]]))
+controls <- as.list(c(1,1,1,4,4,4))
 
-# adjust the control data sets to the absolute mean using the median absolute deviation
-norm_S1_CTRL <- data.frame(norm = S1_CTRL$Alexa488 - mad(S1_CTRL$Alexa488, constant = ctrl_abs_mean))
-norm_S2_CTRL <- data.frame(norm = S2_CTRL$Alexa488 - mad(S2_CTRL$Alexa488, constant = ctrl_abs_mean))
-
-# write the normalization divisors for each set 
-S1_norm <- median(norm_S1_CTRL$norm)
-S2_norm <- median(norm_S2_CTRL$norm)
-
-# normalize the control sets 
-norm_S1_CTRL$norm <- norm_S1_CTRL$norm / S1_norm
-norm_S2_CTRL$norm <- norm_S2_CTRL$norm / S2_norm
-                           
-
-ggplot(data = norm_S1_CTRL, aes(x = norm)) + 
-  geom_bar()
-ggplot(data = norm_S2_CTRL, aes(x = norm)) + 
-  geom_bar()
-
-#---- S1 High dose ----
-S1_HD <- data.frame(data_4h_BT549_gH2aX_S1_HD)
-colnames(S1_HD) <- "Alexa488"
-
-S1_HD$Alexa488 <- signif(log(S1_HD$Alexa488), digits = 4)
-
-ggplot(data = S1_HD, aes(x =Alexa488)) + 
-  geom_bar()
-
-# adjust the data set using the S1 control MAD and normalize using the S1 control factor
-norm_S1_HD <- data.frame(norm = S1_HD$Alexa488 - mad(S1_CTRL$Alexa488, constant = ctrl_abs_mean)) 
-norm_S1_HD$norm <- norm_S1_HD$norm / S1_norm
-
-ggplot(data = norm_S1_HD, aes(x = norm)) + 
-  geom_bar()
-median(norm_S1_HD$norm)
+# adjust the data sets to the absolute mean using the median absolute deviation
+normdata <- list()
+logmads <- list()
+normfactors <- list()
+temp_normdata <- list()
+for (i in seq(length(logdata))){
+  logmads[[i]] <- mad(logdata[[i]], constant = ctrl_abs_mean)
+  temp_normdata[[i]] <- logdata[[i]] - logmads[[controls[[i]]]]
+  normfactors[[i]] <- median(temp_normdata[[controls[[i]]]])
+  normdata[[i]] <- temp_normdata[[i]] / normfactors[[i]]
+}
 
 
-#---- S2 High dose ----
-S2_HD <- data.frame(data_4h_BT549_gH2aX_S2_HD)
-colnames(S2_HD) <- "Alexa488"
 
-S2_HD$Alexa488 <- signif(log(S2_HD$Alexa488), digits = 4)
+#---- Plotting the data ----
 
-ggplot(data = S2_HD, aes(x =Alexa488)) + 
-  geom_bar()
+dfs <- list()
+for (i in seq(length(normdata))){
+  dfs[[i]] <- data.frame(fl = normdata[[i]])
+}
 
+ggplot(dfs[[1]], aes(x = fl)) + 
+  geom_bar(aes(fill = 'Control')) + 
+  geom_bar(data = dfs[[2]], aes(x = fl, fill = 'High Dose')) + 
+  geom_bar(data = dfs[[3]], aes(x = fl, fill = 'Low Dose'))
 
-# adjust the data set using the S1 control MAD and normalize using the S1 control factor
-norm_S2_HD <- data.frame(norm = S2_HD$Alexa488 - mad(S2_CTRL$Alexa488, constant = ctrl_abs_mean)) 
-norm_S2_HD$norm <- norm_S2_HD$norm / S2_norm
-
-ggplot(data = norm_S2_HD, aes(x = norm)) + 
-  geom_bar()
-median(norm_S2_HD$norm)
-
-ggplot(data = norm_S1_CTRL, aes(x = norm)) + 
-  geom_bar(aes(fill = 'red')) + 
-  geom_bar(data = norm_S1_HD, aes(x = norm, fill = 'green'))
-
-S1_norm
-S2_norm
